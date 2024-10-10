1cf8574ffa2f1081db4351a7369459f28652f4f1
kndone <justinlim4916@gmail.com>
2024-10-10 15:38:11 +0800

    feat: PrintInstruction can write to a file

--- src/instructions/PrintInstruction.cpp
+++ src/instructions/PrintInstruction.cpp
@@ -1,10 +1,18 @@
 #include "PrintInstruction.h"
 #include <iostream>
 
-PrintInstruction::PrintInstruction(int pid, std::string& toPrint) : AInstruction(pid, PRINT) {
+PrintInstruction::PrintInstruction(int pid, std::string& toPrint, const std::string filename = "") : AInstruction(pid, PRINT) {
 	this->toPrint = toPrint;
+	this->filename = filename;
 }
 
 void PrintInstruction::execute() {
-	std::cout << "PID " << this->pid << ":" << this->toPrint << "\n";
+	if (this->filename == "") {
+		std::cout << "PID " << this->pid << ":" << this->toPrint << "\n";
+	}
+	else {
+		std::fstream fs(this->filename);
+		fs << "PID " << this->pid << ":" << this->toPrint << "\n";
+		fs.close();
+	}
 }
--- src/instructions/PrintInstruction.h
+++ src/instructions/PrintInstruction.h
@@ -3,14 +3,17 @@
 
 #include "AInstruction.h"
 #include <string>
+#include <fstream>
+#include <optional>
 
 class PrintInstruction : public AInstruction {
 public:
-	PrintInstruction(int pid, std::string& toPrint);
+	PrintInstruction(int pid, std::string& toPrint, const std::string filename="");
 	void execute() override;
 
 private:
 	std::string toPrint;
+	std::string filename;
 };
 
 #endif //PRINTINSTRUCTION_H
