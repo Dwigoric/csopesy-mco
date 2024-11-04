dd701d6b5a3b701e425a80f9003d08aee1e18be7
kndone <justinlim4916@gmail.com>
2024-11-04 22:19:55 +0800

    chore: remove testing code from report util

--- src/commands/ReportUtilCommand.cpp
+++ src/commands/ReportUtilCommand.cpp
@@ -18,19 +18,8 @@ void ReportUtilCommand::printProcesses(std::ostream& os)
         core->mutex.lock();
     }
 
-    os << "(for testing only) NUM PROCESSES: " << SchedulerThread::getInstance()->getProcessList().size() << "\n";
-
     int max_cpu = std::stoi(ConsoleManager::getInstance()->getConfigs()["num-cpu"]);
     int used_cpu = 0;
-
-    for (auto core : CPUManager::getInstance()->getCores()) {
-        auto p = core->getProcess();
-        if (p) {
-            auto timeCreated_time_t = std::chrono::system_clock::to_time_t(p->getTimeExecuted());
-            os << p->getName() << "   " << std::put_time(std::localtime(&timeCreated_time_t), "(%m/%d/%Y %I:%M:%S%p)") << "   Core: " << p->getCore() << "    " <<
-                p->getCurrentInstructionLine() << " / " << p->getMaxInstructionLine() << "\n";
-        }
-    }
 
     for (auto core : CPUManager::getInstance()->getCores()) {
         auto p = core->getProcess();
